e24b613ba745f1ced1c1958a7c92b96ad2f48b4b
Vindymuasa <s21810184@student.unklab.ac.id>
2020-07-06 16:44:55 +0800

    menambahkan pertemuan 18

---
+++ pertemuan18/index.js
@@ -0,0 +1,93 @@
+// console.log("Hello World dari External JS");
+
+
+// let firstName ="Jhon";
+// const $number = 3;
+// let years;
+// var age : 23;
+// let isMerried = false;
+
+let namaPertama ="Mark";
+let namaKedua ="Jhon";
+let num1="150";
+let num2 ="200";
+console.log(namaPertama);
+console.log(namaKedua);
+console.log(num1 === num2);
+console.log(num1 && num2);
+
+ //var massMark=70;
+ //var massJhon=95;
+ //var heightMark= 1.8;
+ //var heigthJhon= 1.75;
+
+ //var BMIMark=massMark/(heigthMark*heightJohn);
+ //var BMIJhon=massJhon/(heightJhon*heightMark);
+ //var isHeigher=BMIMark>BMIJhon;
+ //console.log("Is Jhon's BMI higher than Jhon's ?"+isHigher)
+
+
+// firstName ="Jane";
+// console.log(firstName);
+// console.log($number);
+// console.log(isMerried);
+// console.log(years);
+
+// 2. Operator Aritmatika
+// Operator aritmatika 
+// let num1 = 10;
+// let num2 = 2;
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+//console.log(num1 / num2);
+// console.log(num1 % num2);
+// console.log(num1 ** num2);
+// num1++;
+// console.log(num1);
+// num1--;
+// console.log(num1);
+
+// Assigmnet operator
+// let num3 = 10;
+// let num4;
+// num4 = num3;
+// console.log(num4);
+
+// num3 +=2;
+// console.log(num3);
+
+// String Operator
+// let firstName ="Jhon";
+// let lastName ="Doe";
+// let age = 25;
+// console.log(firstName + " "+ lastName);
+// console.log(firstName + age);
+
+//Operator Perbandingan
+// let num1 = 10;
+
+// console.log(num1 == 10);
+// console.log(num1 =="10");
+
+//console.log(num1 === 10);
+// console.log(num1 ==="10");
+
+// console.log(num1 >= 10);
+// console.log(num1 <= 10);
+
+// let num2 = num1 === 10 ? "Ten" : "Another number";
+//console.log(num2);
+
+//Operator Logika
+//let  hasCertificate = true;
+//let hasTranscript = true;
+
+//console.log(hasCertificate || hasTranscript);
+//console.log(!hasTranscript);
+
+//Operator Precedence
+
+//let result = (10 + 2) * 3; 
+//console.log(result);
+
