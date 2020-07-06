8b9d4f007e57623badb217b19813b555fc0cb840
Vindymuasa <s21810184@student.unklab.ac.id>
2020-07-06 18:04:25 +0800

    menambahkan pertemuan 19

---
+++ pertemuan19/index.js
@@ -0,0 +1,70 @@
+//JavaScript Conditional
+
+ /*console.log("javaScript Conditional");
+
+let firstName = "Jhon";
+let isMarried = true;
+
+if (!isMarried) {
+    console.log(firstName + "will hopefully Married");
+} else {
+    console.log(firstName + "is Married");
+}
+
+let nilaiAndi = 59;
+
+if (nilaiAndi > 80) {
+  console.log("SANGAT MEMUASKAN");
+} else if (nilaiAndi >= 60 && nilaiAndi <= 80) {
+  console.log("MEMUASKAN");
+} else {
+  console.log("JANGAN MENYERAH, COBA LAGI!");
+}
+
+// Switch case
+
+let job ="teacher";
+firstName ="Jhon";
+
+switch (job) {
+    case "teacher" :
+    case "instructor" :
+        console.log(firstName + "is a instructor");
+        break;
+    case "driver" :
+        console.log(firstName + "is a driver");
+        break;
+    case "designer" :
+        console.log(firstName + "is a designer");
+        break;
+    default :
+        console.log(firstName + "does something else");
+        break;
+}
+
+if (job === "instructor") {
+    console.log(firstName + " is a instructor");
+}else if ( job === "driver"){
+    console.log(firstName + " is a driver");
+}else if (job === "designer"){
+    console.log(firstName + " is a designer");
+}else {
+    console.log(firstName + " does something else");
+}*/
+
+//javaScript Function
+
+let firstName ="Jhon";
+
+function calculateAge(birthYear){
+    let age = 2020 - birthYear;
+    console.log(firstName + " age is" + age);
+} 
+
+console.log(firstName);
+console.log(age);
+}
+
+// const calculateAge = function
+// let age = calculateAge(1991);
+// console.log(age);
